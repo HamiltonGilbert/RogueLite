7196c9d7786a82903079e4dd5c24907a9fb418b6
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2022-05-29 22:58:37 -0400

    menu has working description for ships

--- RogueLite/Assets/Game Menu/MenuDescription.cs
+++ RogueLite/Assets/Game Menu/MenuDescription.cs
@@ -5,9 +5,23 @@ using UnityEngine.UI;
 
 public class MenuDescription : MonoBehaviour
 {
-    [SerializeField] Text textBox;
+    [SerializeField] new Text name;
+    [SerializeField] Text description;
+    [SerializeField] Text hp;
+    [SerializeField] Text shield;
+    [SerializeField] Text damage;
+    [SerializeField] Text attackSpeed;
+    [SerializeField] Text moveSpeed;
+    [SerializeField] Text size;
     public void SetDescription(ShipBase ship)
     {
-        textBox.text = ship.Name;
+        name.text = ship.Name;
+        description.text = ship.Description;
+        hp.text = "HP: " + ship.MaxHP;
+        shield.text = "Shield: " + ship.MaxShield;
+        damage.text = "Damage: " + ship.Damage;
+        attackSpeed.text = "Attack Speed: " + ship.AttacksPerSecond * 60;
+        moveSpeed.text = "Move Speed: " + ship.MoveSpeed * 10;
+        size.text = "size: " + ship.SizeMultiplier * 100;
     }
 }
